b86037fd17876becc9cdcd1faf399bed24ed5ae0
kolodindv <71543272+kolodindv@users.noreply.github.com>
2020-12-01 04:11:51 +0400

    Add files via upload

--- SSU.Kolodin_graphClass/SSU.Kolodin_graphClass/Graph.cs
+++ SSU.Kolodin_graphClass/SSU.Kolodin_graphClass/Graph.cs
@@ -21,37 +21,22 @@ namespace SSU.Kolodin_graphClass
         //    get; 
         //}
 
-        public bool Directed
-        {
-            get
-            {
-                return Directed;
-            }
-            set
-            {
-                Directed = value;
-            }
-        }
-        public bool Weighted
-        {
-            get
-            {
-                return Weighted;
-            }
-            set
-            {
-                Weighted = value;
-            }
-        }
+        private bool Directed;
+        private bool Weighted;
+        
 
         //сделать возможность выбирать в файле тип и считывать его
      
 
         private static TType TakeFromString <TType> (string in_)
         {
+            
+            Console.WriteLine("На вход взята: {0}", in_);
             TypeConverter toTname = TypeDescriptor.GetConverter(typeof(TType));
 
-            return (TType)toTname.ConvertFrom(in_);
+            var onon = (TType)toTname.ConvertFrom(in_);
+            Console.WriteLine("На выход ушла: {0}", onon);
+            return onon;
         }
 
         public Graph()
@@ -70,7 +55,8 @@ namespace SSU.Kolodin_graphClass
 
         public Graph(string path)
         {
-            using (StreamReader rd = new StreamReader(path))
+            Encoding windows = Encoding.GetEncoding(1251);
+            using (StreamReader rd = new StreamReader(path, windows))
             {
                 string line = rd.ReadLine();
                 line = rd.ReadLine();
@@ -80,32 +66,43 @@ namespace SSU.Kolodin_graphClass
                 Directed = grProperties.Contains("Directed") ? true : false;
                 Weighted = grProperties.Contains("Weighted") ? true : false;
 
-                string[] special_seps = { " --- ", "|", " |: " };
+                string[] special_seps = { " --- ", "|", " |: ", " "};
                 string[] in_;
 
                 adjList = new Dictionary<Tname, Dictionary<Tname, Tweight>>();
 
+                Tweight digit1 = TakeFromString<Tweight>("1");
+                Console.WriteLine("Создали еденичку");
+
                 while (!rd.EndOfStream)
                 {
-                    in_ = rd.ReadLine().Split(special_seps, StringSplitOptions.RemoveEmptyEntries);                    
+                    in_ = rd.ReadLine().Split(special_seps, StringSplitOptions.RemoveEmptyEntries); 
+                    foreach (var ini in in_)
+                    {
+                        Console.Write("`{0}`", ini);
+                    }
+                    Console.WriteLine();
+
+
                     Tname active_node = TakeFromString<Tname>(in_[0]);
 
+                    Console.WriteLine("Активная вершина {0}", active_node);
 
                     Dictionary<Tname, Tweight> deepAdj = new Dictionary<Tname, Tweight>(); //инициализация субсловаря (глубокого словаря)
 
                     int step = Weighted ? 2 : 1;    //шаг, если граф взвешенный, то будут пары имя смежной вершины + вес (шаг длины 2) 
-                                                    //иначе просто список имен смежных вершин (шаг длины 1)
-
-
-                    Tweight digit1 = TakeFromString<Tweight>("1");
+                                                    //иначе просто список имен смежных вершин (шаг длины 1)                    
 
                     for (int i = 1; i < in_.Length; i += step)
                     {
+
                         deepAdj.Add(TakeFromString<Tname>(in_[i]), Weighted ? TakeFromString<Tweight>(in_[i + 1]) : digit1);
                     }
 
                     adjList.Add(active_node, deepAdj);
                 }
+
+
             }
         }
 
